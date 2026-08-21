223b7165bb9dac29a14e9018574a4008185e05af
Minhaz EsmAil <minhazesmail@gmail.com>
2026-08-21 17:44:11 +0600

    chore(state): remove temporary state bootstrap

--- src/store-init.js
+++
@@ -1,5 +0,0 @@
-import { store } from './eduflow-store.js';
-
-const initialOffline = typeof navigator !== 'undefined' && navigator.onLine === false;
-store.setState({ ui: { offline: initialOffline } }, 'state:init');
-export { store };
